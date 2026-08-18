9f47ab0e08502721e4b5a4f20c9796fa14510257
khldwf94-cyber <khldwf94@gmail.com>
2026-08-19 00:14:21 +0300

    Update verify.js

--- functions/api/verify.js
+++ functions/api/verify.js
@@ -8,6 +8,7 @@ export async function onRequestPost(context) {
       "5886085839",
       "5664645331",
       "1833104668",
+      "7076776569",
     ];
 
     if (!userId || !ALLOWED_IDS.includes(userId)) {
